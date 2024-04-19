3e534b8600bb9c233e56a16c89c08e2c2d802c9a
racine14creator <bisimwagrace596@gmail.com>
2024-04-19 11:52:04 +0200

    add: style

--- data.php
+++ data.php
@@ -2,6 +2,10 @@
 
     <div class="container mx-auto" style="max-width: 1000px; margin: auto;">
         <h3>Data</h3>
+        <div class="box">
+            <h1>Data</h1>
+            <a href="register.php" class="btn btn-md">Add Event</a>
+        </div>
         <?php require_once "./components/Data.php"?>
     </div>
 </body>
--- register.php
+++ register.php
@@ -1,13 +1,6 @@
 <?php include ("./components/Header.php")?>
 
 <div class="container mx-auto" style="min-width: 1000px; margin: auto">
-    <div class="d-flex">
-        <h3><a href="index.php">Racine14 <span>Creator</span></a></h3>
-        <ul>
-            <li><a href="index.php">Dashboard</a></li>
-            <li><a href="data.php">Data</a></li>
-            <li><a href="settings.php">Settings</a></li>
-        </ul>
-    </div>
+    
     <?php include("./components/Registration.php")?>
 </div>
